e359e3c2537af92b2a37cd14a6ea0ad7076b86f9
673637753@qq.com <673637753@qq.com>
2018-05-27 23:56:55 +0800

    demo编写

--- Example/KYCacheAVPlayer/KYViewController.m
+++ Example/KYCacheAVPlayer/KYViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "KYViewController.h"
+#import "KYCacheAVPlayer.h"
 
-@interface KYViewController ()
+@interface KYViewController ()<KYCacheAVPlayerDeleagte>
 
 @end
 
@@ -17,13 +18,47 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // 初始化
+    NSURL *url = [NSURL URLWithString:@"https://mvvideo5.meitudata.com/56ea0e90d6cb2653.mp4"];
+    [KYCacheAVPlayer sharedInstance].delegate = self;
+    [[KYCacheAVPlayer sharedInstance] playVideoWithURL:url];
+    [KYCacheAVPlayer sharedInstance].playerView.videoGravity = AVLayerVideoGravityResize;
+    [self.view addSubview:[KYCacheAVPlayer sharedInstance].playerView];
+    [[KYCacheAVPlayer sharedInstance].playerView setFrame:self.view.bounds];
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSURL *url2 = [NSURL URLWithString:@"http://p11s9kqxf.bkt.clouddn.com/bianche.mp4"];
+        [[KYCacheAVPlayer sharedInstance] playVideoWithURL:url2];
+    });
+    // 切换
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSURL *url2 = [NSURL URLWithString:@"https://mvvideo5.meitudata.com/56ea0e90d6cb2653.mp4"];
+        [[KYCacheAVPlayer sharedInstance] playVideoWithURL:url2];
+    });
+    // 暂停
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[KYCacheAVPlayer sharedInstance] pause];
+    });
+    // 恢复相同地址，则继续播放
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSURL *url2 = [NSURL URLWithString:@"https://mvvideo5.meitudata.com/56ea0e90d6cb2653.mp4"];
+        [[KYCacheAVPlayer sharedInstance] resumeVideoIfNeedWithURL:url2];
+    });
+    // 暂停
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[KYCacheAVPlayer sharedInstance] pause];
+    });
+    // 恢复不同地址，则重新新的地址播放
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSURL *url2 = [NSURL URLWithString:@"http://p11s9kqxf.bkt.clouddn.com/bianche.mp4"];
+        [[KYCacheAVPlayer sharedInstance] resumeVideoIfNeedWithURL:url2];
+    });
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 状态监听 -》 通过委托实现
+- (void)kyAVPlayerStatusChanged:(KYAVPlayerStatus)status{
+    NSLog(@"status: %d",status);
 }
-
+/// 注意页面消失时按需要是否应该移除delegate，防止在其他页面播放回调此处委托
 @end
--- Example/Pods/Target Support Files/Pods-KYCacheAVPlayer_Example/Pods-KYCacheAVPlayer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-KYCacheAVPlayer_Example/Pods-KYCacheAVPlayer_Example-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KYCacheAVPlayer/KYCacheAVPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/VIMediaCache/VIMediaCache.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/KYCacheAVPlayer/KYCacheAVPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/VIMediaCache/VIMediaCache.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
